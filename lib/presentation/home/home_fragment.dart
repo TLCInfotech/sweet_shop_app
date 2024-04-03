57b9d59443bc76a53b802bce74ac31274d5ad9fe
swaroopabawale <swaroopabawale007@gmail.com>
2024-04-03 15:36:52 +0530

    dashboard

--- lib/presentation/home/home_fragment.dart
+++ lib/presentation/home/home_fragment.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:sweet_shop_app/core/colors.dart';
+import 'package:sweet_shop_app/core/common.dart';
 import 'package:sweet_shop_app/core/size_config.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 import '../../core/common_style.dart';
 import '../../core/string_en.dart';
@@ -65,23 +67,78 @@ class _HomeFragmentState extends State<HomeFragment> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                getFieldTitleLayout("Sale,Purchase,Expense"),
-                salepurchasegraph(),
-                const SizedBox(
-                  height: 10,
-                ),
-                getFieldTitleLayout("Also Explore"),
+                // getFieldTitleLayout("Sale,Expense,Return,Recipt"),
+                // salepurchasegraph(),
+                // const SizedBox(
+                //   height: 10,
+                // ),
+                getFieldTitleLayout(" Explore"),
 
-                const SizedBox(
-                  height: 10,
-                ),
+                // const SizedBox(
+                //   height: 10,
+                // ),
                 sale_purchase_expense_container(),
                 const SizedBox(
                   height: 10,
                 ),
-                getFieldTitleLayout("Yearly Report"),
 
-                yearly_report_graph(),
+                // getFieldTitleLayout("Weekly Sale :"),
+                Container(
+                  height: 400,
+                  width: SizeConfig.screenWidth,
+                  child: SfCartesianChart(
+                    title: ChartTitle(text: 'Weekly Sales analysis',alignment: ChartAlignment.near),
+                    // Enable legend
+                    // legend: Legend(isVisible: true),
+                    primaryXAxis: CategoryAxis(),
+                    primaryYAxis: NumericAxis(title: AxisTitle(text: "Sale Amount ",textStyle: item_regular_textStyle )),
+                    series: <ChartSeries>[
+                      ColumnSeries<SalesData, String>(
+                        dataSource: [
+                          SalesData('Mon', 30),
+                          SalesData('Tue', 40),
+                          SalesData('Wed', 35),
+                          SalesData('Thu', 50),
+                          SalesData('Fri', 45),
+                          SalesData('Sat', 60),
+                          SalesData('Sun', 55),
+                        ],
+                        xValueMapper: (SalesData sales, _) => sales.day,
+                        yValueMapper: (SalesData sales, _) => sales.sales,
+                        dataLabelSettings: DataLabelSettings(isVisible: true),
+                      )
+                    ],
+                  ),
+                ),
+                // getFieldTitleLayout("Yearly Report"),
+                Container(
+                  height: 400,
+                  width: 400,
+                  child: SfCircularChart(
+                    title: ChartTitle(text: 'Expense analysis',alignment: ChartAlignment.near),
+                    series: <CircularSeries>[
+                      PieSeries<ExpenseData, String>(
+                        dataSource: [
+                          ExpenseData('Food', 300),
+                          ExpenseData('Rent', 600),
+                          ExpenseData('Transport', 200),
+                          ExpenseData('Utilities', 150),
+                        ],
+                        xValueMapper: (ExpenseData data, _) => data.category,
+                        yValueMapper: (ExpenseData data, _) => data.amount,
+                        dataLabelSettings: DataLabelSettings(
+                          isVisible: true,
+                          connectorLineSettings: ConnectorLineSettings(
+                            color: Colors.blue,
+                            length: '8%',
+                            type: ConnectorType.line,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                // yearly_report_graph(),
               ],
             ),
           ),
@@ -111,17 +168,27 @@ class _HomeFragmentState extends State<HomeFragment> {
               );
   }
 
-  Row sale_purchase_expense_container() {
-    return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  getSellPurchaseExpenseLayout(Colors.green, "10000", "Sell"),
-                  getSellPurchaseExpenseLayout(
-                      Colors.orange, "10000", "purchase"),
-                  getSellPurchaseExpenseLayout(
-                      Colors.deepPurple, "10000", "Expense"),
-                ],
-              );
+   sale_purchase_expense_container() {
+    return Column(
+      children: [
+        Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      getSellPurchaseExpenseLayout(Colors.green, "10000", "Sale"),
+                      getSellPurchaseExpenseLayout(Colors.orange, "10000", "Expense"),
+                      // getSellPurchaseExpenseLayout(Colors.deepPurple, "10000", "Return"),
+                    ],
+                  ),
+        SizedBox(height: 10,),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            getSellPurchaseExpenseLayout(Colors.blue, "10000", "Return"),
+            getSellPurchaseExpenseLayout(Colors.deepPurple, "10000", "Receipt"),
+          ],
+        ),
+      ],
+    );
   }
 
   Container salepurchasegraph() {
@@ -158,11 +225,11 @@ class _HomeFragmentState extends State<HomeFragment> {
       ),
     );
   }
-  Container getSellPurchaseExpenseLayout(
-      MaterialColor boxcolor, String amount, String title) {
-    return Container(
-      height: 170,
-      width: (SizeConfig.screenWidth * 0.85) / 3,
+
+  Container getSellPurchaseExpenseLayout( MaterialColor boxcolor, String amount, String title) {
+    return   Container(
+      height: 120,
+      width: (SizeConfig.screenWidth * 0.85) / 2,
       // margin: EdgeInsets.all(10),
       decoration: BoxDecoration(
           color: boxcolor.withOpacity(0.3),
@@ -172,33 +239,96 @@ class _HomeFragmentState extends State<HomeFragment> {
         children: [
           Container(
             height: 60,
-            width: (SizeConfig.screenWidth * 0.85) / 3,
-            margin: const EdgeInsets.all(15),
+            width: (SizeConfig.screenWidth * 0.85) / 2,
+            // margin: const EdgeInsets.all(15),
             decoration: BoxDecoration(
                 color: boxcolor, borderRadius: BorderRadius.circular(5)),
             alignment: Alignment.center,
             child: Text(
-              "$amount",
-              style: subHeading_withBold,
-            ),
-          ),
-          Text(
-            "$title",
-            style: item_heading_textStyle.copyWith(
-              color: boxcolor,
+              CommonWidget.getCurrencyFormat(double.parse(amount)),
+              style: subHeading_withBold.copyWith(fontSize:18 ),
             ),
           ),
           const SizedBox(
             height: 10,
           ),
-          FaIcon(
-            FontAwesomeIcons.solidArrowAltCircleRight,
-            color: boxcolor,
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "$title",
+                style: item_heading_textStyle.copyWith(
+                  color: boxcolor,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              FaIcon(
+                FontAwesomeIcons.solidArrowAltCircleRight,
+                color: boxcolor,
+              )
+            ],
           )
+
         ],
       ),
     );
+
+    //   Container(
+    //   height: 170,
+    //   width: (SizeConfig.screenWidth * 0.85) / 3,
+    //   // margin: EdgeInsets.all(10),
+    //   decoration: BoxDecoration(
+    //       color: boxcolor.withOpacity(0.3),
+    //       borderRadius: BorderRadius.circular(5)),
+    //   alignment: Alignment.center,
+    //   child: Column(
+    //     children: [
+    //       Container(
+    //         height: 60,
+    //         width: (SizeConfig.screenWidth * 0.85) / 3,
+    //         margin: const EdgeInsets.all(15),
+    //         decoration: BoxDecoration(
+    //             color: boxcolor, borderRadius: BorderRadius.circular(5)),
+    //         alignment: Alignment.center,
+    //         child: Text(
+    //           "$amount",
+    //           style: subHeading_withBold,
+    //         ),
+    //       ),
+    //       Text(
+    //         "$title",
+    //         style: item_heading_textStyle.copyWith(
+    //           color: boxcolor,
+    //         ),
+    //       ),
+    //       const SizedBox(
+    //         height: 10,
+    //       ),
+    //       FaIcon(
+    //         FontAwesomeIcons.solidArrowAltCircleRight,
+    //         color: boxcolor,
+    //       )
+    //     ],
+    //   ),
+    // );
   }
 }
 
 abstract class HomeFragmentInterface {}
+class SalesData {
+  final String day;
+  final int sales;
+
+  SalesData(this.day, this.sales);
+}
+
+class ExpenseData {
+  final String category;
+  final double amount;
+
+  ExpenseData(this.category, this.amount);
+}
